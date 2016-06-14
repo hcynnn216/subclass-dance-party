e75f4866fda9278c718252ab0348de1298a45554
hcynnn216 <yuhongchao@gmail.com>
2016-06-14 13:21:20 -0700

    Solve multiple firings

--- src/init.js
+++ src/init.js
@@ -1,29 +1,33 @@
 $(document).ready(function() {
+
+
   window.dancers = [];
 
-  $('.lineUp').on('click', function(event) {
-console.log('lineUp')
+  $('.lineUp').unbind().click(function(event) {
+    console.log('lineUp')
     $(".dancing").animate({
       "left": "0px"
     }, "slow")
 
 
-  })
+  });
 
-
- //905 1323
-
-//($("body").height()/4) * Math.random())+ .53*$("body").height()
-  $('.addDancerButton').on('click', function(event) {
     
+
+
+
+  //905 1323
+
+  //($("body").height()/4) * Math.random())+ .53*$("body").height()
+  $('.addDancerButton').unbind().click(function(event) {
+
     var dancerMakerFunctionName = $(this).data('dancer-maker-function-name');
 
     var dancerMakerFunction = window[dancerMakerFunctionName];
 
-
     var dancer = new dancerMakerFunction(
 
-      (($("body").height()/3.3) * Math.random())+ 550,
+      (($("body").height() / 3.3) * Math.random()) + 550,
       $("body").width() * Math.random(),
       Math.random() * 1000
     );
@@ -31,44 +35,106 @@ console.log('lineUp')
 
 
     $('body').append(dancer.$node);
+
+
+//$(".dancing").unbind().click( function(event) {
+//      console.log('number of times called');
+
+  //  });
+
+
+
+
+
+
+
+
+
     //
-    
-   //
+
+    //
     window.dancers.push([dancer, dancer.top, dancer.left]);
-    console.log(window.dancers)
+    //console.log(window.dancers)
 
-    $("img").mouseover(function() {
+    $("img").unbind().mouseover(function() {
 
-    //  $(this).css({"border": "10px solid blue"});
+      //  $(this).css({"border": "10px solid blue"});
 
-      $(this).css({"width": "125px","height":"225px"});
+      $(this).css({
+        "width": "125px",
+        "height": "225px"
+      });
     });
 
-    $("img").mouseleave(function() {
+    $("img").unbind().mouseleave(function() {
 
-    //  $(this).css({"border": "10px solid blue"});
+      //  $(this).css({"border": "10px solid blue"});
 
-      $(this).css({"width": "75px","height":"150px"});
+      $(this).css({
+        "width": "75px",
+        "height": "150px"
+      });
     });
 
 
 
- $(".dancing").click(function() {
-console.log('testtt');
+    $(".dancing").unbind().dblclick(function() {
+      Math.random() * window.dancers.length - 1;
 
+
+      console.log('testtt');
+
+
+      $(this).animate({
+        "left": (Math.random() * $("body").width()) + "px"
+      }, "fast")
+
+    });
+
+
+
+
+
+
+   
+    $(".dancing").unbind().click(function() {
+      console.log($(this));
+  
+
+var array=$(this).attr('style').split(' ');
+var stringTop=array[1].slice(0,array[1].length-4);
+var stringLeft=array[3].slice(0,array[3].length-4);
+
+console.log(stringTop,stringLeft)
+
+     
+
+    var index = Math.floor(Math.random() * (window.dancers.length - 1))
+      var person = window.dancers[index];
       
-   $(this).animate({"left": (Math.random()*$("body").width())+ "px"}, "fast")
-      
+      var left = person[2]
+      var top = person[1]
+
+  console.log(top,left);
+
+     
+      $(this).animate({
+        "left": left + "px",
+        "top": top + "px"
+      }, "slow")
+
+      $(this).animate({
+        "left": stringLeft + "px",
+        "top": stringTop + "px"
+      }, "slow")
+
+
+
     });
-
-
-
-
-
-
-
+    
 
 
 
   });
+  
 });
