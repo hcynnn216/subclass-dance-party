17a9730b18fa264f96a01c8a5e9ccb0d4aa79974
hcynnn216 <yuhongchao@gmail.com>
2016-06-13 20:32:31 -0700

    Finish half of dnacing Sprint

--- src/init.js
+++ src/init.js
@@ -1,33 +1,49 @@
 $(document).ready(function() {
   window.dancers = [];
 
-  $('.addDancerButton').on('click', function(event) {
-    /* This function sets up the click handlers for the create-dancer
-     * buttons on dancefloor.html. You should only need to make one small change to it.
-     * As long as the "data-dancer-maker-function-name" attribute of a
-     * class="addDancerButton" DOM node matches one of the names of the
-     * maker functions available in the global scope, clicking that node
-     * will call the function to make the dancer.
-     */
+  $('.lineUp').on('click', function(event) {
+console.log('lineUp')
+    $(".dancing").animate({
+      "left": "0px"
+    }, "slow")
 
-    /* dancerMakerFunctionName is a string which must match
-     * one of the dancer maker functions available in global scope.
-     * A new object of the given type will be created and added
-     * to the stage.
-     */
+
+  })
+
+
+  $('.getBack').on('click', function(event) {
+
+    for (var i = 0; i < window.dancers.length; i++) {
+      var num=window.dancers[i][2]
+      console.log(num+ "px");
+      $(".dancing").animate({"left": num+ "px"}, "fast")
+    }
+  });
+
+
+  $('.addDancerButton').on('click', function(event) {
+    
     var dancerMakerFunctionName = $(this).data('dancer-maker-function-name');
 
-    // get the maker function for the kind of dancer we're supposed to make
     var dancerMakerFunction = window[dancerMakerFunctionName];
 
-    // make a dancer with a random position
 
-    var dancer = dancerMakerFunction(
-      $("body").height() * Math.random(),
+    var dancer = new dancerMakerFunction(
+
+      ($("body").height()) * Math.random(),
       $("body").width() * Math.random(),
       Math.random() * 1000
     );
+
+
+
     $('body').append(dancer.$node);
+    window.dancers.push([dancer, dancer.top, dancer.left]);
+    console.log(window.dancers)
+
+    $(".dancing").mouseover(function() {
+
+      $(this).css("border", "10px solid blue");
+    });
   });
 });
-
